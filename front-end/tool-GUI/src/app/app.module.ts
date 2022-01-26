75d269af264e2564a690777324c41473283a7e58
Jonas Tieppo <jonastieppo@yahoo.com.br>
2022-01-26 10:45:19 -0300

    restarting with css stylesheet

--- front-end/tool-GUI/src/app/app.module.ts
+++ front-end/tool-GUI/src/app/app.module.ts
@@ -3,9 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
@@ -13,9 +10,7 @@ import { MatSliderModule } from '@angular/material/slider';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatSliderModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
