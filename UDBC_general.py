8156bf9264f2682ef949b6564763b437e24b9a20
Jonas Tieppo <jonas.rocha@senairs.org.br>
2024-07-18 10:19:18 -0300

    aplicando bc on hex

--- UDBC_general.py
+++ UDBC_general.py
@@ -2,29 +2,101 @@
 from dataTypes import NodeList, Node
 import pandas as pd
 import numpy as np
-
 class  UDBC_general():
     '''
 
     '''
     def __init__(self, nodes : NodeList) -> None:        
         self.nodeDataFrame = pd.DataFrame(data = nodes)
+        self.nodeDataFrame['n'].astype(int)
+        self.bcDataFrame : list[pd.DataFrame] = []
+        self.__applyBoundaryConditions()
         pass
 
-    def __defineNodeDisplacements(self, nodeInfo : Node, strainTensor: np.array):
-        node_coordinates = np.array([nodeInfo['x'], nodeInfo['y'], nodeInfo['z']])
-        return np.matmul(strainTensor,node_coordinates)
+    def __findCentroid(self):
+        '''
+        Finds the body centroids
+        '''
+        self.x_c = self.nodeDataFrame['x'].mean()
+        self.y_c = self.nodeDataFrame['y'].mean()
+        self.z_c = self.nodeDataFrame['z'].mean()
+
+    def __nodeCoordinatesByCentroid(self, nodeInfo : Node):
+        '''
+        Compute the coordinates relatively to body centoids
+        '''
+        return np.array([nodeInfo['x']-self.x_c, nodeInfo['y']-self.y_c, nodeInfo['z']-self.z_c])
+
+    def __defineNodeDisplacements(self, nodeInfo : Node):
+        node_coordinates = self.__nodeCoordinatesByCentroid(nodeInfo)
+        disp = np.matmul(self.strainTensor,node_coordinates)
+        nodeInfo['u1'] = disp[0]
+        nodeInfo['u2'] = disp[1]
+        nodeInfo['u3'] = disp[2]
+        return nodeInfo
     
-    def __findBoundaryNodes(self, nodes : NodeList):
+    def __findBoundaryNodes(self)->pd.DataFrame:
         '''
         Method to find the boundary nodes bases on node coordinates. It works, however, only for hexadrical-like
         RVE. For an general geometry, further efforts has to de done.
         '''
-        x_max = nodes['x_s'].max()
-        pass
+        x_max = self.nodeDataFrame['x'].max()
+        x_min = self.nodeDataFrame['x'].min()
+        y_max = self.nodeDataFrame['y'].max()
+        y_min = self.nodeDataFrame['y'].min()
+        z_max = self.nodeDataFrame['z'].max()
+        z_min = self.nodeDataFrame['z'].min()
+        
+        boolCondition_x  = np.add(self.nodeDataFrame['x']==x_max,self.nodeDataFrame['x']==x_min)
+        boolCondition_y  = np.add(self.nodeDataFrame['y']==y_max,self.nodeDataFrame['y']==y_min)
+        boolCondition_z  = np.add(self.nodeDataFrame['z']==z_max,self.nodeDataFrame['z']==z_min)
+        boolCondition = np.add(np.add(boolCondition_x,boolCondition_y),boolCondition_z)
+
+        return self.nodeDataFrame[boolCondition]
+
+    def __applyBoundaryConditions(self):
+        '''
+        Method to apply the boundary conditions over a body
+        '''
+        boundaryNodes = self.__findBoundaryNodes()
+        self.__findCentroid()
+
+        for each_strain_case in self.__defineStrainTensors():
+            self.strainTensor = each_strain_case
+            self.bcDataFrame.append(boundaryNodes.apply(self.__defineNodeDisplacements, axis=1))
+
+    def __defineStrainTensors(self)->list[np.array]:
+        '''
+        return a list of strain tensors
+        '''
+        Exx = np.array([[1,0,0],
+                        [0,0,0],
+                        [0,0,0],
+                        ]) 
+        return [Exx]
+
+def createNewMesh(nodesInAdirection = 10):
+    data : NodeList = {} 
+    nnodes = nodesInAdirection**3
+    data['n']=np.linspace(1,nnodes,nnodes).astype('int')
+    data['x']=np.zeros((nnodes))
+    data['y']=np.zeros((nnodes))
+    data['z']=np.zeros((nnodes))
+    counter = 0
+    for z in range(nodesInAdirection):
+        for y in range(nodesInAdirection):
+            for x in range(nodesInAdirection):
+                data['x'][counter] = x
+                data['y'][counter] = y
+                data['z'][counter] = z
+                counter+=1
+
+    return data
 
 
-# teste = UDBC_general(pd.DataFrame())
+if __name__=='__main__':
+    mesh = pd.DataFrame(createNewMesh())
+    teste = UDBC_general(mesh)
 
 # %%
 
--- dataTypes.py
+++ dataTypes.py
@@ -1,13 +1,13 @@
 from typing import TypedDict # for pandas columns annotation
 
 class NodeList(TypedDict):
-    n_s : list[int]   # Node number
-    x_s : list[float] # x coordinate in global coordinate system
-    y_s : list[float] # y coordinate in global coordinate system
-    z_s : list[float] # z coordinate in global coordinate system
-    u1_s : list[float] | None # Applied displacamente on node for D.O.F 1
-    u2_s : list[float] | None # Applied displacamente on node for D.O.F 2
-    u3_s : list[float] | None # Applied displacamente on node for D.O.F 3
+    n : list[int]   # Node number
+    x : list[float] # x coordinate in global coordinate system
+    y : list[float] # y coordinate in global coordinate system
+    z : list[float] # z coordinate in global coordinate system
+    u1 : list[float] | None # Applied displacamente on node for D.O.F 1
+    u2 : list[float] | None # Applied displacamente on node for D.O.F 2
+    u3 : list[float] | None # Applied displacamente on node for D.O.F 3
 
 class Node(TypedDict):
     n : int   # Node number
